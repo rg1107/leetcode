1d598c28ea3f2aecda49a12d7240f9f19409cfd2
rg1107 <37570275+rg1107@users.noreply.github.com>
2024-04-13 00:30:48 +0530

    Time: 60 ms (73.90%) | Memory: 69.4 MB (46.67%) - LeetSync

---
+++ 3363-most-frequent-ids/most-frequent-ids.java
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+        https://leetcode.com/problems/most-frequent-ids/solutions/4916730/c-java-explained-using-heap-and-without-using-heap-very-simple
+     */
+    public long[] mostFrequentIDs(int[] nums, int[] freq) {
+        long[] ans = new long[nums.length];
+        HashMap<Integer, Long> idToFreqMap = new HashMap<>();
+        PriorityQueue<Pair<Long, Integer>> maxHeap = new PriorityQueue<>((a, b) -> Long.compare(b.getKey(), a.getKey()));
+        for(int i = 0; i < nums.length; ++i){
+            idToFreqMap.put(nums[i], idToFreqMap.getOrDefault(nums[i], 0L) + freq[i]);
+            maxHeap.add(new Pair<>(idToFreqMap.get(nums[i]), nums[i]));
+            while(idToFreqMap.get(maxHeap.peek().getValue()) != maxHeap.peek().getKey()) maxHeap.poll();
+            ans[i] = maxHeap.peek().getKey();
+        }
+        return ans;
+    }
+}
