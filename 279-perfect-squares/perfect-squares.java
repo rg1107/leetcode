791ee18dd37f230e0539d772bf1d3af66d06bca0
rg1107 <37570275+rg1107@users.noreply.github.com>
2024-07-24 23:00:38 +0530

    Time: 68 ms (28.41%) | Memory: 42.4 MB (78.10%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int numSquares(int n) {
+        if (n <=3) {
+            return n;
+        }
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
+        dp[4] = 1;
+        return minSquare(n, dp);
+    }
+
+    private int minSquare(int n, int[] dp) {
+        if (dp[n] != -1) {
+            return dp[n];
+        }
+        
+        int rt = (int)Math.sqrt(n);
+        int min = Integer.MAX_VALUE;
+        for (int i = rt; i>=1;i--) {
+            min = Math.min(min, 1 + minSquare(n - i*i, dp));
+        }
+
+        dp[n] = min;
+        return dp[n];
+    }
+}
