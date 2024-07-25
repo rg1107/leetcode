b23ab050baf7513309ff9f7d6ff997559e83c086
rg1107 <37570275+rg1107@users.noreply.github.com>
2024-07-25 23:53:03 +0530

    Time: 1 ms (89.10%) | Memory: 41.4 MB (71.62%) - LeetSync

---
+++ 1298-reverse-substrings-between-each-pair-of-parentheses/reverse-substrings-between-each-pair-of-parentheses.java
@@ -0,0 +1,29 @@
+class Solution {
+    /**
+    https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/solutions/383670/java-c-python-tenet-o-n-solution
+     */
+    public String reverseParentheses(String s) {
+        int n = s.length();
+        Stack<Integer> opened = new Stack<>();
+        int[] pair = new int[n];
+        for (int i = 0; i < n; ++i) {
+            if (s.charAt(i) == '(')
+                opened.push(i);
+            if (s.charAt(i) == ')') {
+                int j = opened.pop();
+                pair[i] = j;
+                pair[j] = i;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0, d = 1; i < n; i += d) {
+            if (s.charAt(i) == '(' || s.charAt(i) == ')') {
+                i = pair[i];
+                d = -d;
+            } else {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
