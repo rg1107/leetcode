b00880aec1842fe002b72d8f927c85d1b4c326e4
rg1107 <37570275+rg1107@users.noreply.github.com>
2024-07-29 09:56:44 +0530

    Time: 6 ms (71.25%) | Memory: 44.6 MB (43.19%) - LeetSync

---
+++ 1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int earliestAcq(int[][] logs, int n) {
+        Arrays.sort(logs, (a,b) -> a[0] - b[0]);
+        int parent[] = new int[n];
+        int rank[] = new int[n];
+        int groups = n;
+
+        for (int i=0;i<n;i++){
+            parent[i] = i;
+            rank[i] = 0;
+        }
+        int ans = -1;
+
+        for (int[] log : logs) {
+            int x = log[1];
+            int y = log[2];
+
+            int parx = findParent(x, parent);
+            int pary = findParent(y, parent);
+
+            if (parx != pary) {
+                if (rank[parx] > rank[pary]) {
+                    parent[pary] = parx;
+                    rank[parx]++;
+                } else {
+                    parent[parx] = pary;
+                    rank[pary]++;
+                }
+                n--;
+            }
+
+            if (n == 1) {
+                ans = log[0];
+                break;
+            }
+
+            // if (areAllFriends(parent)) {
+            //     ans = log[0];
+            //     break;
+            // }
+        }
+
+        return ans;
+    }
+
+    private boolean areAllFriends(int[] parent) {
+        int ans = 0;
+        for (int i=0;i<parent.length;i++) {
+            if(parent[i] == i) {
+                ans++;
+            }
+        }
+        return ans==1;
+    }
+
+    public int findParent(int x, int[] parent) {
+        if (parent[x] == x) {
+            return x;
+        }
+        return findParent(parent[x], parent);
+    }
+}
