5f9693a81e0140e61985f63885213fa529ab7eb6
rg1107 <37570275+rg1107@users.noreply.github.com>
2024-12-03 23:56:06 +0530

    Time: 45 ms (84.36%) | Memory: 48.3 MB (87.00%) - LeetSync

--- 2809-create-hello-world-function/create-hello-world-function.js
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -2,9 +2,9 @@
  * @return {Function}
  */
 var createHelloWorld = function() {
-    
-    return function(...args) {
-        return "Hello World";
+    const greeting = "Hello World";
+    return function() {
+        return greeting;
     }
 };
 
