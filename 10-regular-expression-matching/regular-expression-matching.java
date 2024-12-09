1794d613c224f14c1f3f3707928da50dfb1abf5f
rg1107 <37570275+rg1107@users.noreply.github.com>
2024-12-10 01:31:34 +0530

    Time: 1 ms (100.00%) | Memory: 42.5 MB (18.92%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+    https://leetcode.com/problems/regular-expression-matching/solutions/5847/evolve-from-brute-force-to-dp
+     */
+    Boolean[][] mem;
+    public boolean isMatch(String s, String p) {
+        mem = new Boolean[s.length()+1][p.length()];
+        return isMatch(0,s,0,p);
+    }
+    private boolean isMatch(int i, String s, int j, String p) { 
+        int sn = s.length(), pn = p.length();
+        if(j==pn) { // since * in p can match 0 of previous char, so empty string(i==sn) may match p
+            return i==sn;    
+        }
+        if(mem[i][j]!=null) {
+            return mem[i][j];
+        }
+        char pj = p.charAt(j);
+        if(j+1<pn && p.charAt(j+1)=='*') { //match *, needs to look at the next char to repeate current char
+            if(isMatch(i,s,j+2,p)) {
+                return mem[i][j]=true;
+            }
+            if(i<sn && (pj == '.'||pj==s.charAt(i))) {
+                if(isMatch(i+1,s,j,p)) {
+                    return mem[i][j]=true;
+                }
+            }
+        } else if(i<sn && (s.charAt(i) == pj ||    //match char
+                   pj=='.')) {              //match dot
+            return mem[i][j]=isMatch(i+1, s, j+1, p);
+        }
+        return mem[i][j]=false;
+    }
+}
