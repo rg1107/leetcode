47aa510f0db067d1cfb895f36388e6a0f85fdcd3
rg1107 <37570275+rg1107@users.noreply.github.com>
2025-08-13 20:55:48 +0530

    Time: 918 ms (36.43%) | Memory: 105.1 MB (32.79%) - LeetSync

---
+++ 2321-minimum-weighted-subgraph-with-the-required-paths/minimum-weighted-subgraph-with-the-required-paths.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minimumWeight(self, n: int, edges: List[List[int]], src1: int, src2: int, dest: int) -> int:
+        # https://leetcode.com/problems/minimum-weighted-subgraph-with-the-required-paths/solutions/1844130/python-3-dijkstras-explained
+        G1 = defaultdict(list)
+        G2 = defaultdict(list)
+
+        for a, b ,w in edges:
+            G1[a].append((b,w))
+            G2[b].append((a,w))
+        
+        def Dijkstra(graph, K):
+            q, t = [(0, K)], {}
+            while q:
+                time, node = heappop(q)
+                if node not in t:
+                    t[node] = time
+                    for v, w in graph[node]:
+                        heappush(q, (time+w, v))
+            return [t.get(i, float("inf")) for i in range(n)]
+        
+        arr1 = Dijkstra(G1, src1)
+        arr2 = Dijkstra(G1, src2)
+        arr3 = Dijkstra(G2, dest)
+        
+        ans = float("inf")
+        for i in range(n):
+            ans = min(ans, arr1[i] + arr2[i] + arr3[i])
+        
+        return ans if ans != float("inf") else -1
+        
