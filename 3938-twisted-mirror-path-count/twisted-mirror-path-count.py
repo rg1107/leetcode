d40869e2957ae596db5d83255030482a559214b9
rg1107 <37570275+rg1107@users.noreply.github.com>
2025-09-23 18:21:04 +0530

    Time: 2575 ms (55.02%) | Memory: 163 MB (38.79%) - LeetSync

--- 3938-twisted-mirror-path-count/twisted-mirror-path-count.py
+++ 3938-twisted-mirror-path-count/twisted-mirror-path-count.py
@@ -4,6 +4,24 @@ class Solution:
         n=len(grid[0])
         mod=(10**9)+7
         count=0
+
+        # dp = [[0,0] * n for _ in range(m)]
+        # dp[m-1][n-1][0] = 1
+        # dp[m-1][n-1][1] = 1
+
+        # for row in range(m-1, -1, -1):
+        #     for col in range(n-1, -1, -1):
+        #         for d in range(2):
+        #             if grid[i][j] == 1:
+        #                 if d == 1 and row + 1 < m:
+        #                     dp[row][col] = dp[row + 1][col][0]
+        #                 elif col + 1 < n:
+        #                     dp[row][col] = dp[row][col+1][1]
+        #             else:
+        #                 dp[row][col] = dp[row + 1][col][0] + row[row][col + 1][1]
+        
+        # return dp[0][0] % mod
+
         @cache
         def rec(i,j,d):
             if i==m-1 and j==n-1:
